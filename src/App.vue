9125db8f38879df049ad1bf721f7ccad3f37703e
garguidevsw <garguidevsw@gmail.com>
2023-08-11 09:18:07 -0600

    Bienes Raices App + Vuetify Init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+  >
+    <v-layout>
+      <v-app-bar
+        color="blue-darken-1"
+      >
+        <template v-slot:append>
+          <h1>Bienes Raices</h1>
+        </template>
+        <template v-slot:prepend>
+          <h1>Bienes Raices</h1>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
