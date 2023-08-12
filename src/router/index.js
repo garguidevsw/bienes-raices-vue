fffccb3b769bddab851cfab4b1e1d6b28e052bb1
garguidevsw <garguidevsw@gmail.com>
2023-08-12 11:05:44 -0600

    Add Auth Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useFirebaseAuth } from 'vuefire';
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -18,6 +20,7 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: () => import('../views/admin/AdminLayout.vue'),
+      meta: { requiresAuth: true },
       children: [
         {
           path: '/admin/propiedades',
@@ -38,5 +41,38 @@ const router = createRouter({
     }
   ]
 })
+
+// Guard de navegación
+router.beforeEach(async(to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requiresAuth)
+
+  if(requiresAuth){
+    // Usuario requiere autenticación
+    try {
+      await authenticateUser()
+      next()
+    } catch (error) {
+      console.log(error)
+      next({name: 'login'})
+    }
+  }else{
+    //Usuario no requiere estar autenticado
+    next()
+  }
+})
+
+function authenticateUser() {
+  const auth = useFirebaseAuth()
+  return new Promise((resolve, reject) => {
+    const onsubscribe = onAuthStateChanged(auth, (user) => {
+      onsubscribe()
+      if(user){
+        resolve()
+      }else{
+        reject()
+      }
+    })
+  })
+}
 
 export default router
